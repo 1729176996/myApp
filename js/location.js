616fc66750f3061ba083118c35ff4b1d555549db
1729176996 <1729176996@qq.com>
2019-12-20 15:57:01 +0800

    22

--- js/location.js
+++ js/location.js
@@ -4,7 +4,8 @@ $(function(){
     vm = new Vue({
         el: "#main",
         data:{
-			searchValue:''
+			searchValue:'',
+			basic:[]
         },
         mounted:function(){
             
@@ -26,42 +27,9 @@ $(function(){
 					success:function(data){
 						console.log(data);
 						if(data.HeWeather6&&data.HeWeather6.length>0&&data.HeWeather6[0]&&data.HeWeather6[0].status=='ok'){
-							_this.basic = data.HeWeather6[0].basic?data.HeWeather6[0].basic:{};
-							_this.now = data.HeWeather6[0].now?data.HeWeather6[0].now:{};
-							_this.tab = 'now';
+							_this.basic = data.HeWeather6[0].basic?data.HeWeather6[0].basic:[];
 							_this.$nextTick(function(){
 								mui('#locationwrapper').scroll({
-									deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-								});
-							})
-						}
-					},
-					error:function(xhr, errorType, error,msg){
-						mui.alert(msg,'提示','确定',null,'div');
-					}
-				})
-			},
-			//获取生活指数
-			getWeather_lifestyle:function(){
-				var _this = this;
-				var location = '重庆';
-				if(longitude&&latitude){
-					location = longitude+','+latitude
-				}
-				var url = 'https://free-api.heweather.net/s6/weather/lifestyle?location='+location+'&key='+hftqKey;
-				$.ajax({
-					url:url,
-					type:'GET',
-					dataType:'json',
-					timeout:800,
-					success:function(data){
-						console.log(data);
-						if(data.HeWeather6&&data.HeWeather6.length>0&&data.HeWeather6[0]&&data.HeWeather6[0].status=='ok'){
-							_this.basic = data.HeWeather6[0].basic?data.HeWeather6[0].basic:{};
-							_this.lifestyle = data.HeWeather6[0].lifestyle?data.HeWeather6[0].lifestyle:[];
-							_this.tab = 'lifestyle';
-							_this.$nextTick(function(){
-								mui('#weatherwrapper').scroll({
 									deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
 								});
 							})
